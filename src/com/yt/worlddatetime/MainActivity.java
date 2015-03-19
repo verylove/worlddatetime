8a79aca131b785c57e368013641423b1ac3897cc
verylove <yt5538528@163.com>
2015-03-19 23:08:31 +0800

    增加本地查询时区测试

--- src/com/yt/worlddatetime/MainActivity.java
+++ src/com/yt/worlddatetime/MainActivity.java
@@ -3,22 +3,28 @@ package com.yt.worlddatetime;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	private ListView listView = null;
+	private ListView ListView2 = null;
 	List<Countries> list = new ArrayList<Countries>();
 	private Button button;
 
@@ -94,6 +100,26 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
+				
+				@SuppressWarnings("unused")
+				String[] arrayOfString = TimeZone.getAvailableIDs();
+				List<Map<String, Object>> data = new ArrayList<Map<String,Object>>();  
+				
+				for (int i = 0; i < arrayOfString.length; i++) {
+					Map<String,Object> item;  
+				    item = new HashMap<String,Object>();  
+			        item.put("name",arrayOfString[i].toString());			       
+			        data.add(item);
+			     }
+
+				
+				ListView2 = (ListView) findViewById(R.id.listView2);
+
+				ListAdapter adapter = new SimpleAdapter(getApplicationContext(),  data, android.R.layout.simple_list_item_1, new String[]{"name"}, new int[]{android.R.id.text1});
+
+				ListView2.setAdapter(adapter);
+				 
+				
 				Toast.makeText(getApplicationContext(), "test", 1).show();
 
 			}
